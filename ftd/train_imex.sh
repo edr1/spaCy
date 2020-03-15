6afe674a103a7cc863ab2cba289700e1589fd234
Cloud User <centos@ip-172-31-4-151.us-east-2.compute.internal>
2020-03-15 02:02:46 -0300

    train command

--- ftd/train_imex.sh
+++ ftd/train_imex.sh
@@ -4,4 +4,4 @@
 #python train_new_entity_type.py --model "/home/erickj/Documents/data_f/CODE/spaCy/examples/training/model_imex_en_1" --output-dir "/home/erickj/Documents/data_f/CODE/spaCy/examples/training/model_imex_en_2"
 
 ##python train_ner.py --model "pt_core_news_sm" --output-dir "/home/erickj/Documents/data_f/CODE/spaCy/examples/training/model_imex_pt_1"
-nohup python -u imex-ner.py run --username centos > imex-ner.log 2>&1 & echo $! > centos.pid
+nohup python -u imex-ner.py run --username centos > imex-ner-1m.log 2>&1 & echo $! > centos.pid
